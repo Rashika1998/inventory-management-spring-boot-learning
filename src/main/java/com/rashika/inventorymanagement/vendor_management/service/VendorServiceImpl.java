5131c598476c1872c7b48ead93e8df12001669a6
Rashika1998 <rashikamr1998@gmail.com>
2021-11-04 20:56:16 +0530

    Vendor management APIs integration completed..

--- src/main/java/com/rashika/inventorymanagement/vendor_management/service/VendorServiceImpl.java
+++ src/main/java/com/rashika/inventorymanagement/vendor_management/service/VendorServiceImpl.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class VendorServiceImpl implements VendorService{
@@ -20,5 +21,54 @@ public class VendorServiceImpl implements VendorService{
     @Override
     public List<Vendor> getAllVendors() {
         return vendorRepository.findAll();
+    }
+
+    @Override
+    public Vendor getVendorById(Long vendorId) {
+        return vendorRepository.findById(vendorId).get();
+    }
+
+    @Override
+    public void deleteVendorById(Long vendorId) {
+        vendorRepository.deleteById(vendorId);
+    }
+
+    @Override
+    public Vendor updateVendorById(Long vendorId, Vendor vendor) {
+        Vendor vendorDB = vendorRepository.findById(vendorId).get();
+
+        if (Objects.nonNull(vendor.getVendorCompanyName()) && !"".equalsIgnoreCase(vendor.getVendorCompanyName())){
+            vendorDB.setVendorCompanyName(vendor.getVendorCompanyName());
+        }
+
+        if (Objects.nonNull(vendor.getVendorDesc()) && !"".equalsIgnoreCase(vendor.getVendorDesc())){
+            vendorDB.setVendorDesc(vendor.getVendorDesc());
+        }
+
+        if (Objects.nonNull(vendor.getVendorCompanyAddress()) && !"".equalsIgnoreCase(vendor.getVendorCompanyAddress())){
+            vendorDB.setVendorCompanyAddress(vendor.getVendorCompanyAddress());
+        }
+
+        if (Objects.nonNull(vendor.getVendorAssignDate()) && !"".equalsIgnoreCase(vendor.getVendorAssignDate())){
+            vendorDB.setVendorAssignDate(vendor.getVendorAssignDate());
+        }
+
+        if (Objects.nonNull(vendor.getVendorAgreementEndDate()) && !"".equalsIgnoreCase(vendor.getVendorAgreementEndDate())){
+            vendorDB.setVendorAgreementEndDate(vendor.getVendorAgreementEndDate());
+        }
+
+        if (Objects.nonNull(vendor.isVendorAvailability())){
+            vendorDB.setVendorAvailability(vendor.isVendorAvailability());
+        }
+
+        if (Objects.nonNull(vendor.getVendorDestinationInKm())){
+            vendorDB.setVendorDestinationInKm(vendor.getVendorDestinationInKm());
+        }
+
+        if (Objects.nonNull(vendor.getVendorItemTransportType()) && !"".equalsIgnoreCase(vendor.getVendorItemTransportType())){
+            vendorDB.setVendorItemTransportType(vendor.getVendorItemTransportType());
+        }
+
+        return vendorRepository.save(vendorDB);
     }
 }
